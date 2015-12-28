bb2d6fc4ae8e1f8c7a659dc561d86d15a316e761
JasonChiu <jason1122g@gmail.com>
2015-12-28 22:40:14 +0800

    Add message sender and main

---
+++ killer-server/src/main/java/com/conflux/killer/server/Main.java
@@ -0,0 +1,24 @@
+package com.conflux.killer.server;
+
+import com.conflux.killer.core.message.MessageQueue;
+import com.conflux.killer.core.message.MessageQueueImpl;
+import com.conflux.killer.server.cdc.DataCenter;
+import com.conflux.killer.server.cdc.DataCenterImpl;
+import com.conflux.killer.server.message.*;
+import com.conflux.killer.server.tcp.TCPServerImpl;
+import com.conflux.killer.server.tcp.connection.ConnectionManager;
+
+public class Main {
+    public static void main( String[] args ) {
+        MessageQueue messageQueue = new MessageQueueImpl();
+        ConnectionManager connectionManager = new ConnectionManager( messageQueue );
+        TCPServerImpl server = new TCPServerImpl( connectionManager );
+        server.initTCPServer();
+
+        MessageSender messageSender = new MessageSenderImpl( server );
+        DataCenter dataCenter = new DataCenterImpl( messageSender );
+        MessageReceiver messageReceiver = new MessageReceiverImpl( dataCenter );
+        MessageConsumerThread thread = new MessageConsumerThread( messageQueue, messageReceiver );
+        new Thread( thread ).start();
+    }
+}
--- killer-server/src/main/java/com/conflux/killer/server/message/MessageSenderImpl.java
+++ killer-server/src/main/java/com/conflux/killer/server/message/MessageSenderImpl.java
@@ -2,38 +2,60 @@ package com.conflux.killer.server.message;
 
 import com.conflux.killer.core.message.Direction;
 import com.conflux.killer.core.message.Skill;
+import com.conflux.killer.server.tcp.TCPServer;
 
 import java.awt.*;
 import java.util.Map;
 
 public class MessageSenderImpl implements MessageSender {
+
+    private TCPServer server;
+
+    public MessageSenderImpl( TCPServer server ) {
+        this.server = server;
+    }
+
     @Override
     public void fireAttack( Skill skill, Direction direction, Point point ) {
-
+        String msg = String.format( "1,%s,%s,%d,%d", skill.name(), direction.name(), point.x, point.y );
+        server.sendMessage( msg );
     }
 
     @Override
     public void updatePosition( int clientId, Point point ) {
-
+        String msg = String.format( "2,%d,%d,%d", clientId, point.x, point.y );
+        server.sendMessage( msg );
     }
 
     @Override
     public void removeCharacter( int clientId ) {
-
+        String msg = String.format( "3,%d", clientId );
+        server.sendMessage( msg );
     }
 
     @Override
     public void currentPlayerNumbers( int numbers ) {
-
+        String msg = String.format( "4,%d", numbers );
+        server.sendMessage( msg );
     }
 
     @Override
     public void gameStartWithAllCharacters( Map< Integer, Point > positions ) {
-
+        StringBuilder builder = new StringBuilder( 128 );
+        for ( Map.Entry< Integer, Point > pair : positions.entrySet() ) {
+            builder.append( pair.getKey() )
+                    .append( ":" )
+                    .append( pair.getValue().x )
+                    .append( ":" )
+                    .append( pair.getValue().y )
+                    .append( "," );
+        }
+        String msg = String.format( "5,%s", builder.toString() );
+        server.sendMessage( msg );
     }
 
     @Override
-    public void sendNewClientId( int client ) {
-
+    public void sendNewClientId( int clientId ) {
+        server.sendMessage( "6," + clientId );
     }
 }
