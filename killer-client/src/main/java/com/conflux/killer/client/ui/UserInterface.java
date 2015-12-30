e98d90e550d60ed982610c7671d2f5f1aea052a4
JasonChiu <jason1122g@gmail.com>
2015-12-30 20:23:49 +0800

    Add help

--- killer-client/src/main/java/com/conflux/killer/client/ui/UserInterface.java
+++ killer-client/src/main/java/com/conflux/killer/client/ui/UserInterface.java
@@ -18,7 +18,7 @@ public class UserInterface extends JFrame {
         label.setBounds( 200, 50, 300, 60 );
 
         final JTextField textField = new JTextField();
-        textField.setBounds( 180, 300, 300, 60 );
+        textField.setBounds( 180, 250, 300, 60 );
         textField.setText( "127.0.0.1" );
 
         JButton button = new JButton( "CONNECT" );
@@ -29,9 +29,19 @@ public class UserInterface extends JFrame {
                 new Thread( runnable ).start();
         });
 
+        String helpMessage = "這是一個關於猜疑與生存的遊戲，生存為玩家的第一要務。為了生存，" +
+                "玩家必須先下手為強先殺掉其他對手，因為玩家永遠不知道會不會被背叛、偷襲。\n" +
+                "你可以使用的按鍵有:上,下,左,右,Q,W,E";
+
+        JButton helpBtn = new JButton( "HELP" );
+        helpBtn.setFont( new Font( "Arial", Font.BOLD, 25 ) );
+        helpBtn.setBounds( 180, 320, 300, 60 );
+        helpBtn.addActionListener( (e)-> JOptionPane.showMessageDialog( this, helpMessage ));
+
         JPanel panel = new JPanel();
         panel.setLayout( null );
         panel.add( label );
+        panel.add( helpBtn );
         panel.add( textField );
         panel.add( button );
 
